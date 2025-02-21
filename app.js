7e09ba3be7cf2eb275f3dad487dbd6bbc079a5f5
mustapha <mustaphastar067@gmail.com>
2025-02-21 11:51:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  // 1/first question
+  // let array = countries.length;
+  // console.log(array);
+  //output:11
+  
+  
+  2// second question
+  
+  
+  
+  // get the first element form an array
+  // ===================================
+  // first methode:------
+  console.log(webTechs[0])
+  // second methode:--------
+  let shiftelement = webTechs.shift();
+  console.log(shiftelement);
+  //=====================================
+  
+  // there is shift ans inshifst and pop and push
+  
+  // get the last element form an array
+  // ===================================
+  let length = webTechs.length;
+  let lastElement = webTechs[length-1];
+  console.log(lastElement);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
